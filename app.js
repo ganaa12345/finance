66ee6a59b6dceb03b48addd4cf0744693226bb14
Ganaa <ganaagombo8@gmail.com>
2022-01-06 14:22:33 +0800

    financeControl deer hereglegchiin utgiig hadgalav

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var uiController = (function () {
         inputValue: '.add__value',
         inputButton: '.add__btn'
     }
-
     return {
         getInput: function () {
             return {
@@ -15,37 +14,79 @@ var uiController = (function () {
                 value: document.querySelector(DOMstrings.inputValue).value
             };
         },
-        DOMpublic: function () {
+        getDOMpublic: function () {
             return DOMstrings;
-
         }
     }
 })();
 // sanhvvgiin model controller
 var financeController = (function () {
-})();
-// tovch darah uildel model controller
-var appController = (function (uiCntrl, fnCntrl) {
-    var DOM = uiCntrl.DOMpublic();
-    var ctrlAddItem = function () {
-        console.log(uiController.getInput());
-        // 1. Оруулах өгөгдлийг дэлгэцээс олж авна.
 
-
-        // 2. Олж авсан өгөгдлүүдийн санхүүгийн контроллерт дамжуулж тэнд хадгална.
-        // 3. Олж авсан өгөгдлүүдээ вэб дээрээ тохирох хэсэгт гаргана. 
-        // 4. Төсвийг тооцоолон
-        // 5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
+    var Income = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
     }
 
-    document.querySelector(DOM.inputButton).addEventListener('click', function () {
-        ctrlAddItem();
-    });
-    document.querySelector('.add__value').addEventListener('keypress', function (event) {
-        if (event.keyCode === 13 || event.which === 13) {
-            ctrlAddItem();
+    var Expense = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    }
+
+    var data = {
+        allitems: {
+            inc: [],
+            exp: [],
+        },
+        totals: {
+            inc: 0,
+            exp: 0
         }
-
-    })
-
+    }
+    return {
+        addItem: function (type, desc, val) {
+            var item, id;
+            if (data.allitems[type].length === 0) {
+                id = 1;
+            }
+            else {
+                id = data.allitems[type][data.allitems[type].length - 1].id + 1;
+            }
+            if (type === 'inc') {
+                item = new Income(id, desc, val);
+            } else {
+                item = new Expense(id, desc, val);
+            }
+            data.allitems[type].push(item);
+        },
+        seeData: function () {
+            return data;
+        }
+    };
+})();
+// tovch darah uildel model controller
+var appController = (function (uiController, financeController) {
+    var ctrlAddItem = function () {
+        var input = uiController.getInput();
+        financeController.addItem(input.type, input.description, input.value);
+    };
+    var setupEventListeners = function () {
+        var DOM = uiController.getDOMpublic();
+        document.querySelector(DOM.inputButton).addEventListener('click', function () {
+            ctrlAddItem();
+        });
+        document.querySelector('.add__value').addEventListener('keypress', function (event) {
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        });
+    }
+    return {
+        init: function () {
+            console.log('Application started : ');
+            setupEventListeners();
+        }
+    }
 })(uiController, financeController);
+appController.init();
